b53abd2a6f8cbbb4d15500ddc6c517f06070e400
tgarratt <tomagarratt@yahoo.co.uk>
2020-02-12 11:08:39 +0000

    push so github is up to date

--- order_work/models.py
+++ order_work/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-from django.utils import timezone
-from django import forms
+
 
 class order(models.Model):
 
     title = models.CharField(max_length=50, blank=False)
     company = models.CharField(max_length=50, blank=False, default='')
     brief = models.CharField(max_length=200, blank=False, default='')
-    CATEGORY_CHOICES = (('Logo', 'Logo'), ('Clothing', 'Clothing'), ('WebApp', 'WebApp'), ('Advertising', 'Advertising'), ('Illistration', 'Illistration'), ('Packaging', 'Packaging'))
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='')
+    CATEGORY_CHOICES = (
+        ('25', 'Logo'), ('20', 'Clothing'), ('30', 'WebApp'),
+        ('35', 'Advertising'), ('40', 'Illistration'),
+        ('15', 'Packaging'))
+    category = models.CharField(
+        max_length=50, choices=CATEGORY_CHOICES, default='')
     use = models.CharField(max_length=50, blank=False, default='')
-    WHEN_CHOICES = (('ASAP', 'ASAP',), ('1 week', '1 Week'), ('2 weeks', '2 Weeks'), ('4 weeks+', '4 weeks+'))
+    WHEN_CHOICES = (
+        ('50', 'ASAP',), ('40', '1 Week'), ('30', '2 Weeks'), ('20', '4 weeks+'))
     when = models.CharField(max_length=50, choices=WHEN_CHOICES, default='')
     inspiration = models.URLField(max_length=100, blank=False, default='')
     draft = models.ImageField(upload_to="img/draft", blank=True, null=True)
