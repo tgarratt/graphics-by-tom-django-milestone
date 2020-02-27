6f3f1df316e51f67e1f97ac9fa7f1b2249b10e55
tgarratt <tomagarratt@yahoo.co.uk>
2020-02-27 17:34:59 +0000

    payment now gets the correct price and only addds to admin orders when paid

--- order_work/models.py
+++ order_work/models.py
@@ -24,7 +24,7 @@ class order(models.Model):
     comments = models.CharField(max_length=200, blank=False, default='')
     order_total = models.IntegerField(default=0)
     order_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
-    paid = models.BooleanField(default=True)
+    paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- payment/views.py
+++ payment/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import payment_form
 from order_work.models import order
+from order_work.forms import order_form
 from django.conf import settings
 import stripe
 
@@ -12,15 +13,14 @@ stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def get_payment(request):
-    orders = order.objects.all()
-    name = request.user.username
-    
+    user_order = order.objects.order_by('order_date').last()
+    payment_amount = user_order.order_total
+
     if request.method == "POST":
         form_payment = payment_form(request.POST)
 
         if form_payment.is_valid():
-           
-            price = 10
+            price = payment_amount
             try:
                 customer = stripe.Charge.create(
                     amount=int(price * 100),
@@ -33,13 +33,13 @@ def get_payment(request):
 
             if customer.paid:
                 messages.success(request, "You have successfully paid!")
-                print("success")
+                user_order.paid = True
+                user_order.save()
                 return redirect(reverse("home"))
 
             else:
                 messages.error(request, "Unable to take payment!")
                 
-
         else:
             print(form_payment.errors)
             messages.error(
@@ -49,4 +49,4 @@ def get_payment(request):
         form_payment = payment_form()
 
     return render(
-        request, "../templates/payment.html", {'form_payment': form_payment, 'publishable': settings.STRIPE_PUBLISHABLE, 'orders': orders})
+        request, "../templates/payment.html", {'form_payment': form_payment, 'publishable': settings.STRIPE_PUBLISHABLE, 'payment_amount': payment_amount})
